9c219e98a6cdb264c7b22c2ad32ae70267db67c1
jsf5077 <jsf5077@gmail.com>
2019-06-02 13:57:03 -0400

    basic html setup for the page. using JS I have buttons being created from an array. I can also generate new buttons from the form submit.

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+////////////pseudocode//////////////
+
+//when buttons are clicked, API is called upon to generate gifs
+
+//when user clicks image, the gif animates
+
+//when user click again, animation stops
+
+//when user submits a new button, generate new button and add to the array
+
+//need an array for the inital buttons on the page.
+topics = ["arsenic and old lace", "proof", "seminar", "these shining lives", "epic proportions", "fawlty towers", "she kills monsters", "one flew over the cuckoos nest", "12 angry jurors", "lost in yonkers", "true west" ];
+
+//need a function to generate buttons 
+function renderButtons() {
+    //clear div to reset or else repeats are created everytime the function is called upon
+    $("#buttons-view").empty();
+
+    //create a for loop to generate buttons in the array
+    for (var i = 0; i < topics.length; i++) {
+
+        //generate buttons for each play in the array using jquery.
+        var a = $("<button>");
+        // Add a class
+        a.addClass("play");
+        // Add a data-attribute with a value of the play at index i
+        a.attr("data-name", topics[i]);
+        // Create the button's text with a value of the play at index i
+        a.text(topics[i]);
+        // Add the button to the HTML
+        $("#buttons-view").append(a);
+    }
+}
+
+// $("#find-play").on("click", function(event) {
+
+//     event.preventDefault();
+
+//     var play = $("#data-name").val();
+        ////use search paramters to show the first ten gifs with rating
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=WaAIxnVpQ5UQ7Cn4V85lxhGj63yI9yEg&q="+hello+"&limit=10&offset=0&lang=en"
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response) {
+//       $("#play-view").text(JSON.stringify(response));
+//     });
+//   });
+
+  $("#add-play").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+
+    // Grab the text from the input box
+    var play = $("#play-input").val().trim();
+    // The text from the textbox is then added to our array
+    topics.push(play);
+
+    // reset the buttons on the page
+    renderButtons();
+  });
+
+  renderButtons();
